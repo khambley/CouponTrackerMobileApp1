c31eafb808beb61f0378c75ab5321d187eb2eeeb
Katherine Hambley <superdesigngirl@mac.com>
2022-04-20 19:43:38 -0500

    Added Sqlite and connection in repository

--- CouponTrackerMobileApp1/CouponTrackerMobileApp1/Models/CouponItem.cs
+++ CouponTrackerMobileApp1/CouponTrackerMobileApp1/Models/CouponItem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SQLite;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,12 +7,13 @@ namespace CouponTrackerMobileApp1.Models
 {
     public class CouponItem
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public DateTime ExpirationDate { get; set; }
         public string StoreName { get; set; }
         public string MfrName { get; set; }
         public string ProductName { get; set; }
-        public int Qty { get; set; }
+        public int ProductQty { get; set; }
         public decimal Amount { get; set; }
         public DateTime DateAdded { get; set; }
         public DateTime StartingDate { get; set; }
--- CouponTrackerMobileApp1/CouponTrackerMobileApp1/Repositories/CouponItemRepository.cs
+++ CouponTrackerMobileApp1/CouponTrackerMobileApp1/Repositories/CouponItemRepository.cs
@@ -3,11 +3,46 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using CouponTrackerMobileApp1.Models;
+using System.IO;
 
 namespace CouponTrackerMobileApp1.Repositories
 {
     public class CouponItemRepository : ICouponItemRepository
     {
+        private SQLiteAsyncConnection _connection;
+
+        private async Task CreateConnection()
+        {
+            if (_connection != null)
+            {
+                return;
+            }
+            var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var datebasePath = Path.Combine(documentPath, "CouponItems.db");
+
+            _connection = new SQLiteAsyncConnection(datebasePath);
+
+            await _connection.CreateTableAsync<CouponItem>();
+
+            if(await _connection.Table<CouponItem>().CountAsync() == 0)
+            {
+                await _connection.InsertAsync(new CouponItem()
+                {
+                    ExpirationDate = DateTime.Now.AddDays(15),
+                    StoreName = "Test Store",
+                    ProductName = "Test Product",
+                    ProductQty = 1,
+                    Amount = 3.00M,
+                    DateAdded = DateTime.Now,
+                    StartingDate = DateTime.Now,
+                    Description = "save off any test product 12 oz or higher",
+                    Category = "Health"
+                });
+            }
+        }
+
         public event EventHandler<CouponItem> OnItemAdded;
         public event EventHandler<CouponItem> OnItemUpdated;
 
