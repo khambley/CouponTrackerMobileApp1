55d0f668664a995b28b095369346490d0120ce68
Katherine Hambley <superdesigngirl@mac.com>
2022-04-22 16:08:23 -0500

    Added ViewModelBase to project

---
+++ CouponTrackerMobileApp1/CouponTrackerMobileApp1/ViewModels/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CouponTrackerMobileApp1.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //makes GUI aware of any property changes
+        public void RaisePropertyChanged(params string[] propertyNames)
+        {
+            foreach (var property in propertyNames)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public INavigation Navigation { get; set; }
+    }
+}
