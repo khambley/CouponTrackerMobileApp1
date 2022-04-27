f5d6cea9497751dc7a87a84c768b19c7771abb80
khambley <superdesigngirl@outlook.com>
2022-04-27 10:00:41 -0500

    Wired up the Add button with data binding

--- CouponTrackerMobileApp1/CouponTrackerMobileApp1/ViewModels/CouponPageViewModel.cs
+++ CouponTrackerMobileApp1/CouponTrackerMobileApp1/ViewModels/CouponPageViewModel.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using CouponTrackerMobileApp1.Pages;
+using Xamarin.Forms;
 
 namespace CouponTrackerMobileApp1.ViewModels
 {
@@ -10,12 +13,19 @@ namespace CouponTrackerMobileApp1.ViewModels
     {
         private readonly CouponItemRepository _repository;
 
+        
+
         public CouponPageViewModel(CouponItemRepository repository)
         {
             _repository = repository;
             Task.Run(async () => await LoadData());
         }
 
+        public ICommand AddItem => new Command(async () =>
+        {
+            var couponItem = Resolver.Resolve<CouponItemAddEditPage>();
+            await Navigation.PushAsync(couponItem);
+        });
         private async Task LoadData()
         {
             throw new NotImplementedException();
