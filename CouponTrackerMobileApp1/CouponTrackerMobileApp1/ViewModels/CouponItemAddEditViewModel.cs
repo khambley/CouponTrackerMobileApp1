d5cfaa8e8065ed065637c0c6753792ac978f7595
khambley <superdesigngirl@outlook.com>
2022-04-27 14:49:21 -0500

    Add new items to the list functionaility

--- CouponTrackerMobileApp1/CouponTrackerMobileApp1/ViewModels/CouponItemAddEditViewModel.cs
+++ CouponTrackerMobileApp1/CouponTrackerMobileApp1/ViewModels/CouponItemAddEditViewModel.cs
@@ -1,7 +1,10 @@
-﻿using CouponTrackerMobileApp1.Repositories;
+﻿using CouponTrackerMobileApp1.Models;
+using CouponTrackerMobileApp1.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace CouponTrackerMobileApp1.ViewModels
 {
@@ -9,9 +12,18 @@ namespace CouponTrackerMobileApp1.ViewModels
     {
         private readonly CouponItemRepository _repository;
 
+        public CouponItem Item { get; set; }
+
         public CouponItemAddEditViewModel(CouponItemRepository repository)
         {
             _repository = repository;
+            Item = new CouponItem() { ExpirationDate = DateTime.Now.AddDays(7) };
         }
+
+        public ICommand Save => new Command(async () =>
+        {
+            await _repository.AddOrUpdate(Item);
+            await Navigation.PopAsync();
+        });
     }
 }
