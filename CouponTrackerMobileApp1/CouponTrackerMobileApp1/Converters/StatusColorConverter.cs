8cd8f48c6ba5b52fad4ae5f8abdfe62b54a787fb
khambley <superdesigngirl@outlook.com>
2022-05-09 08:46:51 -0500

    Added StatusColorConverter

---
+++ CouponTrackerMobileApp1/CouponTrackerMobileApp1/Converters/StatusColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CouponTrackerMobileApp1.Converters
+{
+    public class StatusColorConverter : IValueConverter
+    {
+        //This is a comment
+        public StatusColorConverter()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? (Color)Application.Current.Resources["UsedColor"] : (Color)Application.Current.Resources["AvailableColor"];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //used for controls that return data from plain text, like an Entry control.
+            return null;
+        }
+    }
+}
